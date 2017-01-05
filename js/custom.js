4abe7fd699f87fd5272909db2334cc07e7dc006e
Tianhua Fang <tianhuaf0526@gmail.com>
2017-01-04 21:06:57 -0500

    fixed the probelm that tag transparent is delayed

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,7 @@ jQuery(function ($) {
 	$('a.page-scroll').on('click', function (event) {
 		var $anchor = $(this);
 		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top - 10
+			scrollTop: $($anchor.attr('href')).offset().top 
 		}, 1500, 'easeInOutExpo');
 		event.preventDefault();
 	});
